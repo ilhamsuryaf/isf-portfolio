a14af26bd625873e5d6fd445ec9399a0cc998de5
ilhamsuryaf <ilhamsuryafernanda@gmail.com>
2023-11-01 16:05:07 +0700

    refactoring navbar links

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
-    <div className="min-h-screen w-full bg-neutral-800 flex justify-center items-center">
+    <>
       <Navbar />
-      <h1 className="text-3xl font-bold text-white">Hello world!</h1>;
-    </div>
+      <div className="min-h-screen w-full bg-neutral-800 flex justify-center items-center">
+        <h1 className="text-3xl font-bold text-white">Hello world!</h1>;
+      </div>
+    </>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,41 +1,36 @@
 import { useState } from "react";
 import { TiThMenuOutline, TiTimesOutline } from "react-icons/ti";
 
-const Navbar = () => {
-  let [menuOpen, setMenuOpen] = useState(false);
+const navigation = [
+  {
+    title: "About",
+    href: "/about",
+  },
+  {
+    title: "Skill",
+    href: "/skill",
+  },
+  {
+    title: "Project",
+    href: "/project",
+  },
+  {
+    title: "Contact",
+    href: "/contact",
+  },
+];
 
-  const handleMenuOpen = () => {
+const Navbar = () => {
+  let [menuOpen, setMenuOpen] = useState(true);
+
+  const toggleMenu = () => {
     setMenuOpen(!menuOpen);
-    console.log("menu opened");
   };
 
   return (
     <>
-      <div
-        className={`flex fixed justify-around items-center bg-neutral-800 text-white w-full p-4 max-md:h-screen max-md:flex-col max-md:justify-center max-md:shadow-lime-400 max-md:shadow-sm ${
-          !menuOpen ? "max-md:left-0" : "max-md:left-[110%]"
-        } transition-all duration-700 ease-in-out`}
-      >
-        <div className="font-bold text-3xl max-md:absolute max-md:text-9xl max-md:left-1/2 max-md:top-1/2 bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 bg-clip-text text-transparent">
-          <a href="/">ISF</a>
-        </div>
-        <ul className="flex gap-4 font-semibold max-md:block max-md:z-10 max-md:text-3xl max-md:leading-relaxed justify-center">
-          <li>
-            <a href="/about">About</a>
-          </li>
-          <li>
-            <a href="/skill">Skill</a>
-          </li>
-          <li>
-            <a href="/project">Project</a>
-          </li>
-          <li>
-            <a href="/contact">Contact</a>
-          </li>
-        </ul>
-      </div>
       <button
-        onClick={handleMenuOpen}
+        onClick={toggleMenu}
         className="hidden text-white max-md:fixed max-md:z-10 max-md:flex max-md:top-9 max-md:right-9 text-4xl  transition-all duration-500 ease-in-out"
       >
         {!menuOpen ? (
@@ -44,8 +39,39 @@ const Navbar = () => {
           <TiThMenuOutline className="hover:text-green-500" />
         )}
       </button>
+      <div
+        className={`flex fixed justify-around items-center bg-neutral-800 text-white w-full p-4 max-md:h-screen max-md:flex-col max-md:justify-center max-md:shadow-lime-400 max-md:shadow-sm ${
+          !menuOpen ? "max-md:left-0" : "max-md:left-[110%]"
+        } transition-all duration-700 ease-in-out`}
+      >
+        <div className="font-bold text-3xl max-md:absolute max-md:text-9xl max-md:left-1/2 max-md:top-1/2 bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 bg-clip-text text-transparent">
+          <a href="/">ISF</a>
+        </div>
+        <div className="flex gap-4 font-semibold justify-center max-md:block max-md:z-10">
+          {navigation.map((nav, index) => (
+            <NavigationLinks
+              key={index}
+              title={nav.title}
+              href={nav.href}
+            ></NavigationLinks>
+          ))}
+        </div>
+      </div>
     </>
   );
 };
 
 export default Navbar;
+
+const NavigationLinks = ({ title, href }) => {
+  return (
+    <div className="relative max-md:text-4xl max-md:my-4">
+      <a
+        className="after:absolute after:-bottom-1 after:left-0 after:h-1 after:w-0 after:bg-white hover:after:w- after:transition-all after:duration-300"
+        href={href}
+      >
+        {title}
+      </a>
+    </div>
+  );
+};
