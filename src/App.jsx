fd9d5db865a24df6ff9566a0aef1563daaefea44
ilhamsuryaf <ilhamsuryafernanda@gmail.com>
2023-11-06 15:15:15 +0700

    adding footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from "./pages/Hero";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
       <About />
       <Projects />
       <Contact />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className=" py-2 w-full flex justify-center items-center bg-neutral-800">
+      <h3 className="text-white font-semibold opacity-80">
+        &copy; Ilham Surya Fernanda, 2023
+      </h3>
+    </footer>
+  );
+};
+
+export default Footer;
